3fee60fc5050e9e207e86c5fbf5b7b6d1f31de3b
Asus <Asus@karin-lainer>
2015-04-02 10:56:22 +0300

    Wrap Ex 1

---
+++ src/com/lainer/Java_courseServlet.java
@@ -0,0 +1,13 @@
+package com.lainer;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Java_courseServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
