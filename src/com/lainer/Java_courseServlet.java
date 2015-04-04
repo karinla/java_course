67cd27aafcd7b97881248fa6863e426159943d4d
Asus <Asus@karin-lainer>
2015-04-04 14:52:59 +0300

    Exercise 02 ­ Math

--- src/com/lainer/Java_courseServlet.java
+++ src/com/lainer/Java_courseServlet.java
@@ -7,7 +7,18 @@ import javax.servlet.http.*;
 public class Java_courseServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
-	}
+		resp.setContentType("text/html");
+		
+		int Num1 = 4;
+		int Num2 = 3;
+		int Num3 = 7;
+		int result = (Num1*Num3) + (Num2*Num3);
+		
+		String resultStr = new String ("<h1> result of (Num1*Num3) + (Num2*Num3) = "+result+ "</h1>");
+	
+			resp.getWriter().println(resultStr);
+		
+			}
+	
+	
 }
