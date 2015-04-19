f7b632b018e5912088c25e3fbaadbc765d6002c1
Asus <Asus@karin-lainer>
2015-04-19 16:21:58 +0300

    ex03 - math

--- src/com/lainer/Java_courseServlet.java
+++ src/com/lainer/Java_courseServlet.java
@@ -8,17 +8,17 @@ public class Java_courseServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/html");
-		
+
 		int Num1 = 4;
 		int Num2 = 3;
 		int Num3 = 7;
 		int result = (Num1*Num3) + (Num2*Num3);
-		
-		String resultStr = new String ("<h1> result of (Num1*Num3) + (Num2*Num3) = "+result+ "</h1>");
-	
-			resp.getWriter().println(resultStr);
-		
-			}
-	
-	
+
+		String resultStr = new String ("<h1> result of (Num1*Num3) + (Num2*Num3) = " + result + "</h1>");
+
+		resp.getWriter().println(resultStr);
+
+	}
+
+
 }
